ced5cc97dfe8a17bc32dee0aff82cdd142c1ca40
Lewis ZOu <zlzforever@163.com>
2016-04-16 00:07:04 +0800

    update

--- src/Java2Dotnet.Spider.JLog/Log.cs
+++ src/Java2Dotnet.Spider.JLog/Log.cs
@@ -14,152 +14,158 @@ namespace Java2Dotnet.Spider.JLog
         public string Machine = Log.Machine;
         public string UserId = Log.UserId;
         public string TaskId = Log.TaskId;
-        
+
         public override string ToString()
         {
-            return $"[{Type}] {Time} {Machine}-{UserId}-{TaskId} {Message}";    
+            return $"[{Type}] {Time} {Machine}-{UserId}-{TaskId} {Message}";
         }
     }
-    
-	public class Log : ILog
-	{
-		private static readonly object WriteToConsoleLocker = new object();
-		private static readonly object WriteToLogFileLocker = new object();
-		private static readonly string LogFile;
+
+    public class Log : ILog
+    {
+        private static readonly object WriteToConsoleLocker = new object();
+        private static readonly object WriteToLogFileLocker = new object();
+        private static readonly string LogFile;
         public static string UserId { get; set; }
         public static string TaskId { get; set; }
         public static string Machine;
         public static bool NoConsole = false;
-		public string Name { get; }
+        public string Name { get; }
         private static string LogServer;
 
-		static Log()
-		{
+        static Log()
+        {
             Machine = Dns.GetHostName();
-			LogFile = Path.Combine(AppContext.BaseDirectory, DateTime.Now.ToString("yyyy-MM-dd") + ".log");
+            LogFile = Path.Combine(AppContext.BaseDirectory, DateTime.Now.ToString("yyyy-MM-dd") + ".log");
             LogServer = ConfigurationManager.Get("logserver");
-		}
+        }
 
-		public Log(string name)
-		{
-			Name = name;
-		}
+        public Log(string name)
+        {
+            Name = name;
+        }
 
-		public void Warn(string message, Exception e, bool showToConsole)
-		{
+        public void Warn(string message, Exception e, bool showToConsole)
+        {
             var log = CreateLogInfo("WARNING", message, e);
-			WriteToLogFile(log);
-			if (showToConsole)
-			{
-				WriteToConsole(log);
-			}
-		}
-
-		public void Warn(string message, bool showToConsole)
-		{
-			Warn(message, null, showToConsole);
-		}
-
-		public void Info(string message, Exception e, bool showToConsole)
-		{
-            var log = CreateLogInfo("INFO", message, e);
-			WriteToLogFile(log);
-			if (showToConsole)
-			{
-				WriteToConsole(log);
-			}
-		}
-
-		public void Info(string message, bool showToConsole)
-		{
-			Info(message, null, showToConsole);
-		}
-
-		public void Error(string message, Exception e, bool showToConsole)
-		{
-            var log = CreateLogInfo("ERROR", message, e);
-			WriteToLogFile(log);
-			if (showToConsole)
-			{
-				WriteToConsole(log);
-			}
-		}
-
-		public void Error(string message, bool showToConsole)
-		{
-			Error(message, null, showToConsole);
-		}
-
-		public static void WriteLine(string message)
-		{
-            if(NoConsole)
+            WriteToLogFile(log);
+            if (showToConsole)
             {
-                return;    
+                WriteToConsole(log);
             }
-            
-			lock (WriteToConsoleLocker)
-			{				 
-				try
-				{
-					Console.WriteLine(message);
-				}
-				catch
-				{
-				}
-			}
-		}
-		
-		private static void WriteToConsole(LogInfo log)
-		{
-			lock (WriteToConsoleLocker)
-			{
-				switch (log.Type)
-				{
-					case "ERROR":
-						{
-							Console.ForegroundColor = ConsoleColor.Red;
-							break;
-						}
-					case "INFO":
-						{
+        }
+
+        public void Warn(string message, bool showToConsole)
+        {
+            Warn(message, null, showToConsole);
+        }
+
+        public void Info(string message, Exception e, bool showToConsole)
+        {
+            var log = CreateLogInfo("INFO", message, e);
+            WriteToLogFile(log);
+            if (showToConsole)
+            {
+                WriteToConsole(log);
+            }
+        }
+
+        public void Info(string message, bool showToConsole)
+        {
+            Info(message, null, showToConsole);
+        }
+
+        public void Error(string message, Exception e, bool showToConsole)
+        {
+            var log = CreateLogInfo("ERROR", message, e);
+            WriteToLogFile(log);
+            if (showToConsole)
+            {
+                WriteToConsole(log);
+            }
+        }
+
+        public void Error(string message, bool showToConsole)
+        {
+            Error(message, null, showToConsole);
+        }
+
+        public static void WriteLine(string message)
+        {
+            if (NoConsole)
+            {
+                return;
+            }
+
+            lock (WriteToConsoleLocker)
+            {
+                try
+                {
+                    Console.WriteLine(message);
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        private static void WriteToConsole(LogInfo log)
+        {
+            lock (WriteToConsoleLocker)
+            {
+                switch (log.Type)
+                {
+                    case "ERROR":
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            break;
+                        }
+                    case "INFO":
+                        {
                             Console.ForegroundColor = ConsoleColor.Magenta;
-							break;
-						}
-					case "WARNING":
-						{
-							Console.ForegroundColor = ConsoleColor.Yellow;
-							break;
-						}
-				}
-				
-				WriteLine(log.ToString());
-				Console.ForegroundColor = ConsoleColor.White;
-			}
-		}
+                            break;
+                        }
+                    case "WARNING":
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            break;
+                        }
+                }
+
+                WriteLine(log.ToString());
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
 
         private LogInfo CreateLogInfo(string type, string message, Exception e)
         {
             string time = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
-            var log=new LogInfo()
+            var log = new LogInfo()
             {
-                Type=type,
-                Time=time,
+                Type = type,
+                Time = time,
                 Message = message + Environment.NewLine + e
             };
             return log;
         }
-        
-		private static void WriteToLogFile(LogInfo log)
-		{   
-            if(NoConsole && !string.IsNullOrEmpty(LogServer))
+
+        private static void WriteToLogFile(LogInfo log)
+        {
+            if (NoConsole && !string.IsNullOrEmpty(LogServer))
             {
                 HttpClient client = new HttpClient();
-                HttpResponseMessage response = client.PostAsync(LogServer, new StringContent($"{ \"Type\": \"{log.Type}\", \"Time\": \"{log.Time}\", \"Message\": \"{log.Message}\" }")).Result;
-            }        
-			lock (WriteToLogFileLocker)
-			{
-				File.AppendAllText(LogFile, log.ToString(),Encoding.UTF8);
-			}
-		}
-	}
+                StringBuilder builder = new StringBuilder("{ \"Type\": \"");
+                builder.Append(log.Type).Append("\", \"Time\": \"").Append(log.Time).Append("\", \"Message\": \"").Append(log.Message);
+                builder.Append("\", \"Machine\": \"").Append(log.Machine);
+                builder.Append("\", \"UserId\": \"").Append(log.UserId);
+                builder.Append("\", \"TaskId\": \"").Append(log.TaskId);
+                builder.Append("\" }");
+                HttpResponseMessage response = client.PostAsync(LogServer, new StringContent(builder.ToString())).Result;
+            }
+            lock (WriteToLogFileLocker)
+            {
+                File.AppendAllText(LogFile, log.ToString(), Encoding.UTF8);
+            }
+        }
+    }
 }
