7cce4f1f6cbbae50af1f1418ded300ec8e4d0268
Lewis Zou <zlzforever@163.com>
2016-05-11 16:08:30 +0800

    update

--- src/Java2Dotnet.Spider.Extension/Model/Attribute/Downloader.cs
+++ src/Java2Dotnet.Spider.Extension/Model/Attribute/Downloader.cs
@@ -2,7 +2,5 @@
 {
 	public class Downloader : System.Attribute
 	{
-		public Configuration.Downloader.Types Type { get; set; }
-		public object Arguments { get; set; }
 	}
 }
--- src/Java2Dotnet.Spider.Extension/Model/Formatter/IObjectFormatter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Java2Dotnet.Spider.Extension.Model.Formatter
-{
-	public interface IObjectFormatter
-	{
-		IObjectFormatter NextFormatter { get; set; }
-		dynamic Format(string raw);
-		void InitParam(string[] extra);
-	}
-}
