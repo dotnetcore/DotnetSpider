e8d09c1e9af15d4ee1393eb363341ff3bb0895cf
Walterwhatwater <weichentang0607@163.com>
2016-06-08 18:10:05 +0800

    TrimFormatter

---
+++ src/Java2Dotnet.Spider.Extension/Model/Formatter/TrimFormater.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Java2Dotnet.Spider.Extension.Model.Formatter
+{
+	[AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+	public class TrimFormater : Formatter
+	{
+		public override string Name { get; internal set; } = "TrimFormatter";
+
+		public override string Formate(string value)
+		{
+			return value?.Trim();
+		}
+	}
+}
