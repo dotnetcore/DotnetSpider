eb1036ffd74328d8ad187d853a19346e89dc37b3
Lewis Zou <zlzforever@163.com>
2016-07-20 15:16:14 +0800

    fix build issue

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ dotnet restore
 dotnet pack src/HtmlAgilityPack/project.json -o spider_nuget_packages 
 dotnet pack src/HtmlAgilityPack.Css/project.json -o spider_nuget_packages
 dotnet pack src/MySql.Data/project.json -o spider_nuget_packages
-dotnet pack src/Newtonsoft.Json/project.json -o spider_nuget_packages
+dotnet pack src/StackExchange.Redis/project.json -o spider_nuget_packages
 dotnet pack src/Java2Dotnet.Spider.Common/project.json -o spider_nuget_packages
 dotnet pack src/Java2Dotnet.Spider.Ioc/project.json -o spider_nuget_packages
 dotnet pack src/Java2Dotnet.Spider.Log/project.json -o spider_nuget_packages
