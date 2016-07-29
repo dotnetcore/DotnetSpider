04cb8e67e99c479e6a34a239f1a01e3e5ee9715b
user <user@DESKTOP-R9TVIN7>
2016-07-29 09:37:41 +0800

    udpate

--- src/Java2Dotnet.Spider.Test/Example/JdSkuSample.cs
+++ src/Java2Dotnet.Spider.Test/Example/JdSkuSample.cs
@@ -28,7 +28,7 @@ namespace Java2Dotnet.Spider.Test.Example
 			});
 			context.AddPipeline(new MysqlPipeline
 			{
-				ConnectString = "Database='test';Data Source=86research.imwork.net;User ID=root;Password=1qazZAQ!;Port=4306"
+				ConnectString = "Database='test';Data Source=mysqlserver;User ID=root;Password=1qazZAQ!;Port=4306"
 			});
 			context.AddStartUrl("http://list.jd.com/list.html?cat=9987,653,655&page=2&JL=6_0_0&ms=5#J_main", new Dictionary<string, object> { { "name", "手机" }, { "cat3", "655" } });
 			context.AddEntityType(typeof(Product));
