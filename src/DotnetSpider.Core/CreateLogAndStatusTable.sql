823c715e65240fb0e2e218bfa6ca06217e81f0cf
Lewis Zou <zlzforever@163.com>
2016-08-24 17:49:15 +0800

    update script

--- src/DotnetSpider.Core/CreateLogAndStatusTable.sql
+++ src/DotnetSpider.Core/CreateLogAndStatusTable.sql
@@ -1,9 +1,9 @@
 ﻿CREATE DATABASE IF NOT EXISTS `dotnetspider`  DEFAULT CHARACTER SET utf8;
 CREATE TABLE IF NOT EXISTS `dotnetspider`.`log` (
-  `identity` varchar(100) DEFAULT NULL,
-  `taskgroup` varchar(30) DEFAULT NULL,
-  `userid` varchar(30) DEFAULT NULL,
-  `logged` varchar(30) DEFAULT NULL,
+  `identity` varchar(200) DEFAULT NULL,
+  `taskgroup` varchar(200) DEFAULT NULL,
+  `userid` varchar(200) DEFAULT NULL,
+  `logged` timestamp NULL DEFAULT NULL,
   `level` varchar(10) DEFAULT NULL,
   `message` text,
   `callSite` text,
@@ -13,9 +13,9 @@ CREATE TABLE IF NOT EXISTS `dotnetspider`.`log` (
   KEY `index01` (`identity`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1950900 DEFAULT CHARSET=utf8;
 CREATE TABLE IF NOT EXISTS `dotnetspider`.`status` (
-  `taskgroup` varchar(30) DEFAULT NULL,
-  `userid` varchar(30) DEFAULT NULL,
-  `identity` varchar(50) DEFAULT NULL,
+  `taskgroup` varchar(200) DEFAULT NULL,
+  `userid` varchar(200) DEFAULT NULL,
+  `identity` varchar(200) DEFAULT NULL,
   `status` varchar(20) DEFAULT NULL,
   `message` varchar(200) DEFAULT NULL,
   `logged` timestamp NULL DEFAULT NULL,
