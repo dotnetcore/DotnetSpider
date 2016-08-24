28453f4d03b40ef1dcfb1be0b0da951cca4898bf
Lewis Zou <zlzforever@163.com>
2016-08-24 16:06:18 +0800

    Fix issue

--- src/DotnetSpider.Extension/SpiderBuilder.cs
+++ src/DotnetSpider.Extension/SpiderBuilder.cs
@@ -1,4 +1,3 @@
-using System;
 #if Test
 using Newtonsoft.Json;
 #endif
@@ -7,25 +6,19 @@ namespace DotnetSpider.Extension
 {
 	public abstract class EntitySpiderBuilder
 	{
-		protected Action VerifyCollectedData { get; set; }
-
 		protected abstract EntitySpider GetEntitySpider();
 
 		public virtual void Run(params string[] args)
 		{
 			var spider = GetEntitySpider();
-			if (spider != null)
-			{
 #if Test
 	// 转成JSON再转换成SpiderContext, 用于测试JsonSpiderContext是否正常
 			string json = JsonConvert.SerializeObject(GetSpiderContext());
 			ModelSpider spider = new ModelSpider(JsonConvert.DeserializeObject<JsonSpiderContext>(json).ToRuntimeContext());
 #elif Publish
-				//ModelSpider spider = new ModelSpider(context) {AfterSpiderFinished = AfterSpiderFinished};
+			//ModelSpider spider = new ModelSpider(context) {AfterSpiderFinished = AfterSpiderFinished};
 #endif
-				spider.VerifyCollectedData = VerifyCollectedData;
-				spider.Run(args);
-			}
+			spider?.Run(args);
 		}
 	}
 }
