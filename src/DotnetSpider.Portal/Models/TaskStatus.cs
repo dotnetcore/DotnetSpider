46d9d3a71c988a5df0db1010c62b1daac73c717a
Lewis Zou <zlzforever@163.com>
2016-08-30 17:41:48 +0800

    remove useless codes

--- src/DotnetSpider.Portal/Models/TaskStatus.cs
+++ src/DotnetSpider.Portal/Models/TaskStatus.cs
@@ -53,24 +53,5 @@ namespace DotnetSpider.Portal.Models
 		public string Message { get; set; }
 		public DateTime Logged { get; set; }
 		public Int64 Id { get; set; }
-
-		public static List<TaskStatus> Create(int num)
-		{
-			var list = new List<TaskStatus>();
-			for (int i = 0; i < num; ++i)
-			{
-				list.Add(new TaskStatus
-				{
-					Id = i,
-					TaskGroup = "YY",
-					UserId = "86Research",
-					Identity = "YY Channel " + i,
-					Status = StatusCode.Finished,
-					Message = "Left: 0 Total: 100 Success: 100 Error: 0",
-					Logged = DateTime.Now
-				});
-			}
-			return list;
-		}
 	}
 }
