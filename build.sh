acced3e1e4fc275a0cd53ac0f9dab1e11ec2908b
Lewis Zou <zlzforever@163.com>
2016-11-02 13:04:19 +0800

    更新编译脚本

--- build.sh
+++ build.sh
@@ -1,37 +1,14 @@
 #!/bin/sh
 rm -rf ~/solutions/DotnetSpider/spider_nuget_packages
 dotnet restore
-dotnet build src/DotnetSpider.Extension/project.json -f netcoreapp1.0
+dotnet build src/DotnetSpider.Extension/project.json
 dotnet pack src/DotnetSpider.HtmlAgilityPack/project.json -o ~/solutions/DotnetSpider/spider_nuget_packages --no-build
 dotnet pack src/DotnetSpider.HtmlAgilityPack.Css/project.json -o ~/solutions/DotnetSpider/spider_nuget_packages --no-build
 dotnet pack src/DotnetSpider.Core/project.json -o ~/solutions/DotnetSpider/spider_nuget_packages --no-build
 dotnet pack src/DotnetSpider.Redial/project.json -o ~/solutions/DotnetSpider/spider_nuget_packages --no-build
 dotnet pack src/DotnetSpider.Extension/project.json -o ~/solutions/DotnetSpider/spider_nuget_packages --no-build
-ftp -n<<!
-open redis
-user ftpuser 1qazZAQ!
-binary
-hash
-lcd ~/solutions/DotnetSpider/spider_nuget_packages
-cd /nuget
-prompt
-mdelete /nuget/DotnetSpider.HtmlAgilityPack/0.0.9/*
-rmdir /nuget/DotnetSpider.HtmlAgilityPack/0.0.9
-rmdir /nuget/DotnetSpider.HtmlAgilityPack
-mdelete /nuget/DotnetSpider.HtmlAgilityPack.Css/0.0.9/*
-rmdir /nuget/DotnetSpider.HtmlAgilityPack.Css/0.0.9
-rmdir /nugetDotnetSpider./HtmlAgilityPack.Css
-mdelete /nuget/DotnetSpider.Redial/0.0.9/*
-rmdir /nuget/DotnetSpider.Redial/0.0.9
-rmdir /nuget/DotnetSpider.Redial
-mdelete /nuget/DotnetSpider.Core/0.0.9/*
-rmdir /nuget/DotnetSpider.Core/0.0.9
-rmdir /nuget/DotnetSpider.Core
-mdelete /nuget/DotnetSpider.Extension/0.0.9/*
-rmdir /nuget/DotnetSpider.Extension/0.0.9
-rmdir /nuget/DotnetSpider.Extension
-mput *
-delete 86research02.cache.bin
-close
-bye
-! 
+nuget push DotnetSpider.HtmlAgilityPack.0.9.1.nupkg
+nuget push DotnetSpider.HtmlAgilityPack.Css.0.9.1.nupkg
+nuget push DotnetSpider.Core.0.9.1.nupkg
+nuget push DotnetSpider.Redial.0.9.1.nupkg
+nuget push DotnetSpider.Extension.0.9.1.nupkg
