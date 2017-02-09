1c04a7843ab65558410229ee37860b2e88fee5a9
Lewis Zou <zlzforever@163.com>
2017-02-09 17:45:28 +0800

    update

--- src/DotnetSpider2.Core/Resource.Designer.cs
+++ src/DotnetSpider2.Core/Resource.Designer.cs
@@ -38,7 +38,7 @@ namespace DotnetSpider.Core {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DotnetSpider.Core.Resource", typeof(Resource).GetTypeInfo().Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DotnetSpider2.Core.Resource", typeof(Resource).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
