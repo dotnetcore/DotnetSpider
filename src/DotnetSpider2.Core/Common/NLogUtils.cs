2bea2ee15c8f3769f77f6aaf849ec538038c9376
Lewis Zou <zlzforever@163.com>
2017-02-09 18:00:40 +0800

    restruct default nlog config

--- src/DotnetSpider2.Core/Common/NLogUtils.cs
+++ src/DotnetSpider2.Core/Common/NLogUtils.cs
@@ -21,7 +21,7 @@ namespace DotnetSpider.Core.Common
 #endif
 				if (!File.Exists(nlogConfigPath))
 				{
-					File.AppendAllText(nlogConfigPath, Resource.nlog);
+					File.AppendAllText(nlogConfigPath, NlogDefaultConfig);
 				}
 				XmlLoggingConfiguration configuration = new XmlLoggingConfiguration(nlogConfigPath);
 				var connectString = Configuration.GetValue("logAndStatusConnectString");
@@ -44,11 +44,13 @@ namespace DotnetSpider.Core.Common
 					configuration.RemoveTarget("dblog");
 					configuration.RemoveTarget("dbstatus");
 				}
- 
+
 				configuration.Install(new InstallationContext());
 				LogManager.Configuration = configuration;
 				_init = true;
 			}
 		}
+
+		private const string NlogDefaultConfig = "<nlog xmlns=\"http://www.nlog-project.org/schemas/NLog.xsd\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" throwExceptions=\"true\"><targets><target name = \"console\" xsi:type=\"ColoredConsole\" useDefaultRowHighlightingRules=\"true\" layout=\"[${date:format=yyyyMMdd HH\\:mm\\:ss}][${event-properties:item=UserId}][${event-properties:item=TaskGroup}][${level}] ${message}\"><highlight-row foregroundColor = \"Cyan\" condition=\"level == LogLevel.Trace\"/><highlight-row foregroundColor = \"DarkGray\" condition=\"level == LogLevel.Debug\"/></target><target name = \"file\" xsi:type=\"File\" maxArchiveFiles=\"30\" layout=\"[${date:format=yyyyMMdd HH\\:mm\\:ss}][${event-properties:item=UserId}][${event-properties:item=TaskGroup}][${level}] ${message}\" fileName=\"${basedir}/logs/log${shortdate}.txt\" keepFileOpen=\"false\" /></targets><rules><logger name = \"*\" minlevel=\"Trace\" writeTo=\"console\" /><logger name = \"*\" minlevel=\"Trace\" writeTo=\"file\" /></rules></nlog>";
 	}
 }
