8d2c47a2148ae68cd3532904a9fbf6c4c951a36a
Lewis Zou <zlzforever@163.com>
2017-02-09 16:09:58 +0800

    重命名程序集, 因为NUGET上名称被占用

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
 set version_suffix=""
 del /q/a/f/s c:\solutions\DotnetSpider\spider_nuget_packages\*.*
 dotnet restore
-dotnet build c:\solutions\DotnetSpider\src\DotnetSpider.Extension\project.json
-dotnet pack c:\solutions\DotnetSpider\src\DotnetSpider.HtmlAgilityPack\project.json -o c:\solutions\DotnetSpider\spider_nuget_packages --no-build --version-suffix %version_suffix%
-dotnet pack c:\solutions\DotnetSpider\src\DotnetSpider.HtmlAgilityPack.Css\project.json -o c:\solutions\DotnetSpider\spider_nuget_packages --no-build --version-suffix %version_suffix%
-dotnet pack c:\solutions\DotnetSpider\src\DotnetSpider.Core\project.json -o c:\solutions\DotnetSpider\spider_nuget_packages --no-build --version-suffix %version_suffix%
-dotnet pack c:\solutions\DotnetSpider\src\DotnetSpider.Redial\project.json -o c:\solutions\DotnetSpider\spider_nuget_packages --no-build --version-suffix %version_suffix%
-dotnet pack c:\solutions\DotnetSpider\src\DotnetSpider.Extension\project.json -o c:\solutions\DotnetSpider\spider_nuget_packages --no-build --version-suffix %version_suffix%
+dotnet build c:\solutions\DotnetSpider\src\DotnetSpider2.Extension\project.json
+dotnet pack c:\solutions\DotnetSpider\src\DotnetSpider2.HtmlAgilityPack\project.json -o c:\solutions\DotnetSpider\spider_nuget_packages --no-build --version-suffix %version_suffix%
+dotnet pack c:\solutions\DotnetSpider\src\DotnetSpider2.HtmlAgilityPack.Css\project.json -o c:\solutions\DotnetSpider\spider_nuget_packages --no-build --version-suffix %version_suffix%
+dotnet pack c:\solutions\DotnetSpider\src\DotnetSpider2.Core\project.json -o c:\solutions\DotnetSpider\spider_nuget_packages --no-build --version-suffix %version_suffix%
+dotnet pack c:\solutions\DotnetSpider\src\DotnetSpider2.Redial\project.json -o c:\solutions\DotnetSpider\spider_nuget_packages --no-build --version-suffix %version_suffix%
+dotnet pack c:\solutions\DotnetSpider\src\DotnetSpider2.Extension\project.json -o c:\solutions\DotnetSpider\spider_nuget_packages --no-build --version-suffix %version_suffix%
 for %%i in (C:\solutions\DotnetSpider\spider_nuget_packages\*.symbols.nupkg) do del /q/a/f/s %%i
---
+++ src/DotnetSpider2.Extension/Pipeline/MongoDbEntityPipeline.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using DotnetSpider.Core;
+using DotnetSpider.Extension.Model;
+using DotnetSpider.Extension.ORM;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DotnetSpider.Extension.Pipeline
+{
+	public class MongoDbEntityPipeline : BaseEntityPipeline
+	{
+		public string ConnectString { get; set; }
+		[JsonIgnore]
+		public IUpdateConnectString UpdateConnectString { get; set; }
+		protected Schema Schema { get; set; }
+		private IMongoCollection<BsonDocument> _collection;
+
+		public MongoDbEntityPipeline(string connectString)
+		{
+			ConnectString = connectString;
+		}
+
+		public override void InitiEntity(EntityMetadata metadata)
+		{
+			if (metadata.Schema == null)
+			{
+				Spider.Log("Miss pipeline because: Schema is necessary", LogLevel.Warn);
+				IsEnabled = false;
+				return;
+			}
+
+			Schema = BaseEntityDbPipeline.GenerateSchema(metadata.Schema);
+			MongoClient client = new MongoClient(ConnectString);
+			var db = client.GetDatabase(metadata.Schema.Database);
+
+			_collection = db.GetCollection<BsonDocument>(metadata.Schema.TableName);
+		}
+
+		public override void Process(List<JObject> datas)
+		{
+			List<BsonDocument> reslut = new List<BsonDocument>();
+			foreach (var data in datas)
+			{
+				BsonDocument item = BsonDocument.Parse(data.ToString());
+
+				reslut.Add(item);
+			}
+			_collection.InsertMany(reslut);
+		}
+
+		public override BaseEntityPipeline Clone()
+		{
+			return new MongoDbEntityPipeline(ConnectString)
+			{
+				UpdateConnectString = UpdateConnectString
+			};
+		}
+
+		public Schema GetSchema()
+		{
+			return Schema;
+		}
+	}
+}
