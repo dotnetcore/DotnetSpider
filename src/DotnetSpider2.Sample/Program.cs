e9f0178e02688d5c57cf4b13b8e3e7d466d78873
Lewis Zou <zlzforever@163.com>
2017-02-23 11:27:07 +0800

    update samples

--- src/DotnetSpider2.Sample/Program.cs
+++ src/DotnetSpider2.Sample/Program.cs
@@ -16,51 +16,51 @@ namespace DotnetSpider.Sample
 			Console.WriteLine("Press any key to continue...");
 			Console.Read();
 
-			// Custmize processor and pipeline 完全自定义页面解析和数据管道
-			BaseUsage.CustmizeProcessorAndPipeline();
-			Console.WriteLine("Press any key to continue...");
-			Console.Read();
+			//// Custmize processor and pipeline 完全自定义页面解析和数据管道
+			//BaseUsage.CustmizeProcessorAndPipeline();
+			//Console.WriteLine("Press any key to continue...");
+			//Console.Read();
 
-			// Crawler pages without traverse 采集指定页面不做遍历
-			BaseUsage.CrawlerPagesWithoutTraverse();
-			Console.WriteLine("Press any key to continue...");
-			Console.Read();
+			//// Crawler pages without traverse 采集指定页面不做遍历
+			//BaseUsage.CrawlerPagesWithoutTraverse();
+			//Console.WriteLine("Press any key to continue...");
+			//Console.Read();
 
-			// Crawler pages traversal 遍历整站
-			BaseUsage.CrawlerPagesTraversal();
-			Console.WriteLine("Press any key to continue...");
-			Console.Read();
+			//// Crawler pages traversal 遍历整站
+			//BaseUsage.CrawlerPagesTraversal();
+			//Console.WriteLine("Press any key to continue...");
+			//Console.Read();
 
-			DDengEntitySpider dDengEntitySpider = new DDengEntitySpider();
-			dDengEntitySpider.Run();
-			Console.WriteLine("Press any key to continue...");
-			Console.Read();
+			//DDengEntitySpider dDengEntitySpider = new DDengEntitySpider();
+			//dDengEntitySpider.Run();
+			//Console.WriteLine("Press any key to continue...");
+			//Console.Read();
 
-			Cnblogs.Run();
-			Console.WriteLine("Press any key to continue...");
-			Console.Read();
+			//Cnblogs.Run();
+			//Console.WriteLine("Press any key to continue...");
+			//Console.Read();
 
-			CasSpider casSpider = new CasSpider();
-			casSpider.Run();
-			Console.WriteLine("Press any key to continue...");
-			Console.Read();
+			//CasSpider casSpider = new CasSpider();
+			//casSpider.Run();
+			//Console.WriteLine("Press any key to continue...");
+			//Console.Read();
 
-			BaiduSearchSpider baiduSearchSpider = new BaiduSearchSpider();
-			baiduSearchSpider.Run();
-			Console.WriteLine("Press any key to continue...");
-			Console.Read();
+			//BaiduSearchSpider baiduSearchSpider = new BaiduSearchSpider();
+			//baiduSearchSpider.Run();
+			//Console.WriteLine("Press any key to continue...");
+			//Console.Read();
 
-			JdShopDetailSpider jdShopDetailSpider = new JdShopDetailSpider();
-			jdShopDetailSpider.Run();
-			Console.WriteLine("Press any key to continue...");
-			Console.Read();
+			//JdShopDetailSpider jdShopDetailSpider = new JdShopDetailSpider();
+			//jdShopDetailSpider.Run();
+			//Console.WriteLine("Press any key to continue...");
+			//Console.Read();
 
-			JdSkuSampleSpider jdSkuSampleSpider = new JdSkuSampleSpider();
-			jdSkuSampleSpider.Run();
-			Console.WriteLine("Press any key to continue...");
-			Console.Read();
+			//JdSkuSampleSpider jdSkuSampleSpider = new JdSkuSampleSpider();
+			//jdSkuSampleSpider.Run();
+			//Console.WriteLine("Press any key to continue...");
+			//Console.Read();
 
-			Situoli.Run();
+			//Situoli.Run();
 		}
 	}
 }
