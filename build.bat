a1602d1a70f66a8cf48727d341eed9e9e3e5b15b
Lewis Zou <zlzforever@163.com>
2017-06-20 22:36:08 +0800

    update nuget package

--- build.bat
+++ build.bat
@@ -9,5 +9,6 @@ for %%i in (*.nupkg) do del /q/a/f/s %%i
 rem create nuget packages
 nuget pack DotnetSpider.Core.nuspec
 nuget pack DotnetSpider.Extension.nuspec
+nuget pack DotnetSpider.Runner.nuspec
 rem upload nuget packages
 for %%i in (*.nupkg) do nuget push %%i -Source https://www.nuget.org/api/v2/package
