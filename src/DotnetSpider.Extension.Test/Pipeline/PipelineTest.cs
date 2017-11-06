39b00b7d5a5d33bdff4517b5859fbe73778ad554
邹嵩 <zousong@research.xbmail.com.cn>
2017-11-06 11:27:48 +0800

    混用EntitySpider和自定义Pipeline, Processor时, 若未添加EntityPipeline, 提示异常

---
+++ src/DotnetSpider.Extension.Test/Pipeline/PipelineTest.cs
@@ -0,0 +1,122 @@
+﻿using Dapper;
+using DotnetSpider.Core;
+using DotnetSpider.Core.Pipeline;
+using DotnetSpider.Core.Processor;
+using DotnetSpider.Core.Selector;
+using DotnetSpider.Extension.Model;
+using DotnetSpider.Extension.Model.Attribute;
+using DotnetSpider.Extension.Model.Formatter;
+using DotnetSpider.Extension.Pipeline;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DotnetSpider.Extension.Test.Pipeline
+{
+	public class PipelineTest
+	{
+		[Fact]
+		public void MixProcessorAndMissEntityPipeline()
+		{
+			var exceptoin = Assert.Throws<SpiderException>(() =>
+			{
+				var id = Guid.NewGuid().ToString("N");
+				BaiduSearchSpider spider = new BaiduSearchSpider();
+				spider.Identity = id;
+				spider.Run();
+			});
+			Assert.Equal("You may miss a entity pipeline.", exceptoin.Message);
+		}
+
+		[Fact]
+		public void MixProcessor()
+		{
+			var id = Guid.NewGuid().ToString("N");
+			BaiduSearchSpider spider = new BaiduSearchSpider();
+			spider.AddPipeline(new MySqlEntityPipeline());
+			spider.Identity = id;
+			spider.Run();
+
+			using (var conn = new MySqlConnection(Env.DataConnectionString))
+			{
+				var count = conn.QueryFirst<int>("SELECT COUNT(*) FROM baidu.baidu_search_mixprocessor");
+				Assert.Equal(20, count);
+				conn.Execute("DROP TABLE baidu.baidu_search_mixprocessor");
+			}
+		}
+
+
+		public class BaiduSearchSpider : EntitySpider
+		{
+			public BaiduSearchSpider() : base("BaiduSearchSpider")
+			{
+			}
+
+			protected override void MyInit(params string[] arguments)
+			{
+				var word = "可乐|雪碧";
+				AddStartUrl(string.Format("http://news.baidu.com/ns?word={0}&tn=news&from=news&cl=2&pn=0&rn=20&ct=1", word), new Dictionary<string, dynamic> { { "Keyword", word } });
+				EmptySleepTime = 1000;
+				AddEntityType<BaiduSearchEntry>();
+				AddPageProcessor(new MyProcessor());
+				AddPipeline(new MyPipeline());
+			}
+
+			class MyPipeline : BasePipeline
+			{
+				public override void Process(params ResultItems[] resultItems)
+				{
+				}
+			}
+
+			class MyProcessor : BasePageProcessor
+			{
+				protected override void Handle(Page page)
+				{
+				}
+			}
+
+			[EntityTable("baidu", "baidu_search_mixprocessor")]
+			[EntitySelector(Expression = ".//div[@class='result']", Type = SelectorType.XPath)]
+			class BaiduSearchEntry : SpiderEntity
+			{
+				[PropertyDefine(Expression = "Keyword", Type = SelectorType.Enviroment)]
+				public string Keyword { get; set; }
+
+				[PropertyDefine(Expression = ".//h3[@class='c-title']/a")]
+				[ReplaceFormatter(NewValue = "", OldValue = "<em>")]
+				[ReplaceFormatter(NewValue = "", OldValue = "</em>")]
+				public string Title { get; set; }
+
+				[PropertyDefine(Expression = ".//h3[@class='c-title']/a/@href")]
+				public string Url { get; set; }
+
+				[PropertyDefine(Expression = ".//div/p[@class='c-author']/text()")]
+				[ReplaceFormatter(NewValue = "-", OldValue = "&nbsp;")]
+				public string Website { get; set; }
+
+
+				[PropertyDefine(Expression = ".//div/span/a[@class='c-cache']/@href")]
+				public string Snapshot { get; set; }
+
+
+				[PropertyDefine(Expression = ".//div[@class='c-summary c-row ']", Option = PropertyDefine.Options.PlainText)]
+				[ReplaceFormatter(NewValue = "", OldValue = "<em>")]
+				[ReplaceFormatter(NewValue = "", OldValue = "</em>")]
+				[ReplaceFormatter(NewValue = " ", OldValue = "&nbsp;")]
+				public string Details { get; set; }
+
+				[PropertyDefine(Expression = ".", Option = PropertyDefine.Options.PlainText)]
+				[ReplaceFormatter(NewValue = "", OldValue = "<em>")]
+				[ReplaceFormatter(NewValue = "", OldValue = "</em>")]
+				[ReplaceFormatter(NewValue = " ", OldValue = "&nbsp;")]
+				public string PlainText { get; set; }
+
+				[PropertyDefine(Expression = "today", Type = SelectorType.Enviroment)]
+				public DateTime run_id { get; set; }
+			}
+		}
+	}
+}
--- src/DotnetSpider.Extension/EntitySpider.cs
+++ src/DotnetSpider.Extension/EntitySpider.cs
@@ -7,68 +7,72 @@ using System.Linq;
 
 namespace DotnetSpider.Extension
 {
-    public abstract class EntitySpider : CommonSpider
-    {
-        public EntitySpider() : this(new Site())
-        {
-        }
+	public abstract class EntitySpider : CommonSpider
+	{
+		public EntitySpider() : this(new Site())
+		{
+		}
 
-        public EntitySpider(Site site) : base(site)
-        {
-        }
+		public EntitySpider(Site site) : base(site)
+		{
+		}
 
-        public EntitySpider(string name) : base(name)
-        {
-        }
+		public EntitySpider(string name) : base(name)
+		{
+		}
 
-        public EntitySpider(string name, Site site) : base(name, site)
-        {
-        }
+		public EntitySpider(string name, Site site) : base(name, site)
+		{
+		}
 
-        public void AddEntityType<T>(string tableName = null) where T : ISpiderEntity
-        {
-            AddEntityType<T>(null, tableName);
-        }
+		public void AddEntityType<T>(string tableName = null) where T : ISpiderEntity
+		{
+			AddEntityType<T>(null, tableName);
+		}
 
-        public void AddEntityType<T>(DataHandler<T> dataHandler) where T : ISpiderEntity
-        {
-            AddEntityType(dataHandler, null);
-        }
+		public void AddEntityType<T>(DataHandler<T> dataHandler) where T : ISpiderEntity
+		{
+			AddEntityType(dataHandler, null);
+		}
 
-        public void AddEntityType<T>(DataHandler<T> dataHandler, string tableName) where T : ISpiderEntity
-        {
-            CheckIfRunning();
+		public void AddEntityType<T>(DataHandler<T> dataHandler, string tableName) where T : ISpiderEntity
+		{
+			CheckIfRunning();
 
-            EntityProcessor<T> processor = new EntityProcessor<T>(Site, dataHandler, tableName);
-            AddPageProcessor(processor);
-        }
+			EntityProcessor<T> processor = new EntityProcessor<T>(Site, dataHandler, tableName);
+			AddPageProcessor(processor);
+		}
 
-        protected override IPipeline GetDefaultPipeline()
-        {
-            return BaseEntityPipeline.GetPipelineFromAppConfig();
-        }
+		protected override IPipeline GetDefaultPipeline()
+		{
+			return BaseEntityPipeline.GetPipelineFromAppConfig();
+		}
 
-        protected override void PreInitComponent(params string[] arguments)
-        {
-            base.PreInitComponent(arguments);
+		protected override void PreInitComponent(params string[] arguments)
+		{
+			base.PreInitComponent(arguments);
 
-            if (arguments.Contains("skip"))
-            {
-                return;
-            }
+			if (arguments.Contains("skip"))
+			{
+				return;
+			}
 
-            foreach (var processor in PageProcessors)
-            {
-                if (processor is IEntityProcessor entityProcessor)
-                {
-                    foreach (var pipeline in Pipelines)
-                    {
-                        BaseEntityPipeline newPipeline = pipeline as BaseEntityPipeline;
+			var entityProcessors = PageProcessors.Where(p => p is IEntityProcessor).ToList();
+			var entityPipelines = Pipelines.Where(p => p is BaseEntityPipeline).ToList();
 
-                        newPipeline?.AddEntity(entityProcessor.EntityDefine);
-                    }
-                }
-            }
-        }
-    }
+			if (entityProcessors.Count != 0 && entityPipelines.Count == 0)
+			{
+				throw new SpiderException("You may miss a entity pipeline.");
+			}
+			foreach (var processor in entityProcessors)
+			{
+				foreach (var pipeline in entityPipelines)
+				{
+					var entityProcessor = processor as IEntityProcessor;
+					BaseEntityPipeline newPipeline = pipeline as BaseEntityPipeline;
+					newPipeline.AddEntity(entityProcessor.EntityDefine);
+				}
+			}
+		}
+	}
 }
