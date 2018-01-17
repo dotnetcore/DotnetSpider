56653671f771e0569e9c32a849f69b50052c751d
moonheart <fzp1205561571@hotmail.com>
2018-01-17 22:58:38 +0800

    translations for BaseDownloader

--- src/DotnetSpider.Core/Downloader/BaseDownloader.cs
+++ src/DotnetSpider.Core/Downloader/BaseDownloader.cs
@@ -7,40 +7,55 @@ using System.Runtime.CompilerServices;
 
 namespace DotnetSpider.Core.Downloader
 {
-	/// <summary>
+    /// <summary>
+    /// The Abstraction of a basic downloader.
+    /// </summary>
+    /// <summary xml:lang="zh-CN">
 	/// 基础下载器的抽象
-	/// </summary>
-	public abstract class BaseDownloader : Named, IDownloader
+    /// </summary>
+    public abstract class BaseDownloader : Named, IDownloader
 	{
-		/// <summary>
+        /// <summary>
+        /// Log interface.
+        /// </summary>
+        /// <summary xml:lang="zh-CN">
 		/// 日志接口
-		/// </summary>
-		protected static readonly ILogger Logger = DLog.GetLogger();
+        /// </summary>
+        protected static readonly ILogger Logger = DLog.GetLogger();
 
-		/// <summary>
+        /// <summary>
+        /// Whether the downloader should automaticlly detect <see cref="Site.ContentType"/>.
+        /// </summary>
+        /// <summary xml:lang="zh-CN">
 		/// 是否检测过下载内容的类型
-		/// </summary>
-		private bool _detectedContentType;
+        /// </summary>
+        private bool _detectedContentType;
 		private readonly List<IAfterDownloadCompleteHandler> _afterDownloadCompletes = new List<IAfterDownloadCompleteHandler>();
 		private readonly List<IBeforeDownloadHandler> _beforeDownloads = new List<IBeforeDownloadHandler>();
 
-		/// <summary>
+        /// <summary>
+        /// Cookie Container
+        /// </summary>
+        /// <summary xml:lang="zh-CN">
 		/// Cookie 容器
-		/// </summary>
-		protected readonly CookieContainer CookieContainer = new CookieContainer();
+        /// </summary>
+        protected readonly CookieContainer CookieContainer = new CookieContainer();
 
 		/// <summary>
 		/// Interface to inject cookie.
 		/// </summary>
 		public ICookieInjector CookieInjector { get; set; }
 
-		/// <summary>
+        /// <summary>
+        /// Add cookies to downloader
+        /// </summary>
+        /// <summary xml:lang="zh-CN">
 		/// 设置 Cookies
-		/// </summary>
-		/// <param name="cookiesStr">Cookies的键值对字符串, 如: a1=b;a2=c;</param>
-		/// <param name="domain">作用域</param>
-		/// <param name="path">作用路径</param>
-		public void AddCookies(string cookiesStr, string domain, string path = "/")
+        /// </summary>
+        /// <param name="cookiesStr">Cookies的键值对字符串, 如: a1=b;a2=c;(Cookie's key-value pairs string, a1=b;a2=c; etc.)</param>
+        /// <param name="domain">作用域(<see cref="Cookie.Domain"/>)</param>
+        /// <param name="path">作用路径(<see cref="Cookie.Path"/>)</param>
+        public void AddCookies(string cookiesStr, string domain, string path = "/")
 		{
 			var cookies = new Dictionary<string, string>();
 			var pairs = cookiesStr.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
@@ -53,12 +68,15 @@ namespace DotnetSpider.Core.Downloader
 			}
 		}
 
-		/// <summary>
-		/// 添加Cookies
-		/// </summary>
-		/// <param name="cookies">Cookies的键值对</param>
-		/// <param name="domain">作用域</param>
-		/// <param name="path">作用路径</param>
+        /// <summary>
+        /// Add cookies to downloader
+        /// </summary>
+        /// <summary xml:lang="zh-CN">
+        /// 添加Cookies
+        /// </summary>
+        /// <param name="cookies">Cookies的键值对 (Cookie's key-value pairs)</param>
+        /// <param name="domain">作用域(<see cref="Cookie.Domain"/>)</param>
+        /// <param name="path">作用路径(<see cref="Cookie.Path"/>)</param>
 		public void AddCookies(IDictionary<string, string> cookies, string domain, string path = "/")
 		{
 			foreach (var pair in cookies)
@@ -69,30 +87,36 @@ namespace DotnetSpider.Core.Downloader
 			}
 		}
 
-		/// <summary>
-		/// 添加Cookie
-		/// </summary>
-		/// <param name="name">名称</param>
-		/// <param name="value">值</param>
-		/// <param name="domain">作用域</param>
-		/// <param name="path">作用路径</param>
+        /// <summary>
+        /// Add one cookie to downloader
+        /// </summary>
+        /// <summary xml:lang="zh-CN">
+        /// 添加Cookie
+        /// </summary>
+        /// <param name="name">名称(<see cref="Cookie.Name"/>)</param>
+        /// <param name="value">值(<see cref="Cookie.Value"/>)</param>
+        /// <param name="domain">作用域(<see cref="Cookie.Domain"/>)</param>
+        /// <param name="path">作用路径(<see cref="Cookie.Path"/>)</param>
 		public void AddCookie(string name, string value, string domain, string path = "/")
 		{
 			var cookie = new Cookie(name.Trim(), value.Trim(), path.Trim(), domain.Trim());
 			AddCookie(cookie);
 		}
-		
-		/// <summary>
-		/// Gets a System.Net.CookieCollection that contains the System.Net.Cookie instances that are associated with a specific URI.
-		/// </summary>
-		/// <param name="uri">The URI of the System.Net.Cookie instances desired.</param>
-		/// <returns>A System.Net.CookieCollection that contains the System.Net.Cookie instances that are associated with a specific URI.</returns>
-		public CookieCollection GetCookies(Uri uri)
+
+        /// <summary>
+        /// Gets a <see cref="System.Net.CookieCollection"/> that contains the <see cref="System.Net.Cookie"/> instances that are associated with a specific <see cref="Uri"/>.
+        /// </summary>
+        /// <param name="uri">The URI of the System.Net.Cookie instances desired.</param>
+        /// <returns>A <see cref="System.Net.CookieCollection"/> that contains the <see cref="System.Net.Cookie"/> instances that are associated with a specific <see cref="Uri"/>.</returns>
+        public CookieCollection GetCookies(Uri uri)
 		{
 			return CookieContainer.GetCookies(uri);
 		}
 
-		/// <summary>
+	    /// <summary>
+	    /// Add one cookie to downloader
+	    /// </summary>
+	    /// <summary xml:lang="zh-CN">
 		/// 设置 Cookie
 		/// </summary>
 		/// <param name="cookie">Cookie</param>
@@ -106,21 +130,27 @@ namespace DotnetSpider.Core.Downloader
 			AddCookieToDownloadClient(cookie);
 		}
 
-		/// <summary>
+        /// <summary>
+        /// Override this method to add cookies to Downloaders (HttpClient, WebDriver etc.) 
+        /// </summary>
+        /// <summary xml:lang="zh-CN">
 		/// 下载器在第一次使用时, 会把CookieContainer中的Cookie加载到下载工具中(HttpClient, WebDriver), 
-		/// 但当下载器已经在运行时, 更新Cookie则需要使用此方法把新的Cookie更新到各个下载工具中
-		/// </summary>
-		/// <param name="cookie">Cookie</param>
-		[MethodImpl(MethodImplOptions.Synchronized)]
+        /// 但当下载器已经在运行时, 更新Cookie则需要使用此方法把新的Cookie更新到各个下载工具中
+        /// </summary>
+        /// <param name="cookie">Cookie</param>
+        [MethodImpl(MethodImplOptions.Synchronized)]
 		protected virtual void AddCookieToDownloadClient(Cookie cookie) { }
 
-		/// <summary>
+        /// <summary>
+        /// Download webpage content and build a <see cref="Page"/> instance.
+        /// </summary>
+        /// <summary xml:lang="zh-CN">
 		/// 下载链接内容
-		/// </summary>
-		/// <param name="request">链接请求</param>
-		/// <param name="spider">爬虫接口</param>
-		/// <returns>下载内容封装好的页面对象</returns>
-		public Page Download(Request request, ISpider spider)
+        /// </summary>
+        /// <param name="request">链接请求 <see cref="Request"/></param>
+        /// <param name="spider">爬虫接口 <see cref="ISpider"/></param>
+        /// <returns>下载内容封装好的页面对象 (a <see cref="Page"/> instance that contains requested page infomations, like Html source, headers, etc.)</returns>
+        public Page Download(Request request, ISpider spider)
 		{
 			BeforeDownload(ref request, spider);
 			var page = DowloadContent(request, spider);
@@ -129,29 +159,38 @@ namespace DotnetSpider.Core.Downloader
 			return page;
 		}
 
-		/// <summary>
+        /// <summary>
+        /// Add a <see cref="IAfterDownloadCompleteHandler"/> to <see cref="IDownloader"/>
+        /// </summary>
+        /// <summary xml:lang="zh-CN">
 		/// 添加处理器
-		/// </summary>
-		/// <param name="handler"><see cref="IAfterDownloadCompleteHandler"/></param>
-		public void AddAfterDownloadCompleteHandler(IAfterDownloadCompleteHandler handler)
+        /// </summary>
+        /// <param name="handler"><see cref="IAfterDownloadCompleteHandler"/></param>
+        public void AddAfterDownloadCompleteHandler(IAfterDownloadCompleteHandler handler)
 		{
 			_afterDownloadCompletes.Add(handler);
 		}
 
-		/// <summary>
+        /// <summary>
+        /// Add a <see cref="IBeforeDownloadHandler"/> to <see cref="IDownloader"/>
+        /// </summary>
+        /// <summary xml:lang="zh-CN">
 		/// 添加处理器
-		/// </summary>
-		/// <param name="handler"><see cref="IBeforeDownloadHandler"/></param>
-		public void AddBeforeDownloadHandler(IBeforeDownloadHandler handler)
+        /// </summary>
+        /// <param name="handler"><see cref="IBeforeDownloadHandler"/></param>
+        public void AddBeforeDownloadHandler(IBeforeDownloadHandler handler)
 		{
 			_beforeDownloads.Add(handler);
 		}
 
-		/// <summary>
+        /// <summary>
+        /// Clone a Downloader throuth <see cref="object.MemberwiseClone"/>, override if you need a deep clone or others. 
+        /// </summary>
+        /// <summary xml:lang="zh-CN">
 		/// 克隆一个下载器, 多线程时, 每个线程使用一个下载器, 这样如WebDriver下载器则不再需要管理WebDriver对象的个数了, 每个下载器就只包含一个WebDriver。
-		/// </summary>
-		/// <returns>下载器</returns>
-		public virtual IDownloader Clone()
+        /// </summary>
+        /// <returns>下载器</returns>
+        public virtual IDownloader Clone()
 		{
 			return MemberwiseClone() as IDownloader;
 		}
@@ -163,13 +202,16 @@ namespace DotnetSpider.Core.Downloader
 		{
 		}
 
-		/// <summary>
+        /// <summary>
+        /// Override this method to download content.
+        /// </summary>
+        /// <summary xml:lang="zh-CN">
 		/// 下载工作的具体实现
-		/// </summary>
-		/// <param name="request">请求信息</param>
-		/// <param name="spider">爬虫</param>
-		/// <returns>页面数据</returns>
-		protected abstract Page DowloadContent(Request request, ISpider spider);
+        /// </summary>
+        /// <param name="request">请求信息 <see cref="Request"/></param>
+        /// <param name="spider">爬虫 <see cref="ISpider"/></param>
+        /// <returns>页面数据 <see cref="Page"/></returns>
+        protected abstract Page DowloadContent(Request request, ISpider spider);
 
 		private void BeforeDownload(ref Request request, ISpider spider)
 		{
@@ -193,12 +235,12 @@ namespace DotnetSpider.Core.Downloader
 			}
 		}
 
-		/// <summary>
-		/// TODO: 应该考虑使用更高效的检测方法, 以及是否有方法可以去掉同步锁
-		/// </summary>
-		/// <param name="page"></param>
-		/// <param name="spider"></param>
-		[MethodImpl(MethodImplOptions.Synchronized)]
+        /// <summary>
+        /// Try to detect Content type
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="spider"></param>
+        [MethodImpl(MethodImplOptions.Synchronized)]
 		private void TryDetectContentType(Page page, ISpider spider)
 		{
 			if (!_detectedContentType)
