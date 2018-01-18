29c95ceaa818c61d368979f52b093e623088d9b1
moonheart <fzp1205561571@hotmail.com>
2018-01-18 22:00:31 +0800

    translation for DownloadException

--- src/DotnetSpider.Core/Downloader/DownloadException.cs
+++ src/DotnetSpider.Core/Downloader/DownloadException.cs
@@ -1,11 +1,17 @@
 ﻿namespace DotnetSpider.Core.Downloader
 {
 	/// <summary>
+	/// Exception that <see cref="IDownloader"/> throws.
+	/// </summary>
+	/// <summary xml:lang="zh-CN">
 	/// 下载器抛出的异常
 	/// </summary>
 	public class DownloadException : SpiderException
 	{
 		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <summary xml:lang="zh-CN">
 		/// 构造方法
 		/// </summary>
 		public DownloadException() : base("Download Exception")
@@ -13,9 +19,12 @@
 		}
 
 		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <summary xml:lang="zh-CN">
 		/// 构造方法
 		/// </summary>
-		/// <param name="message">异常信息</param>
+		/// <param name="message">异常信息 Error message</param>
 		public DownloadException(string message) : base(message)
 		{
 		}
