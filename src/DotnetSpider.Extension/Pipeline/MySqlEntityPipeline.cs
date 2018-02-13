8744f07e3cf8f0f472ef9db14ddbd186ce814b75
CQWY <CQWY@CQWY-PC>
2018-02-13 16:12:13 +0800

    修改GenerateInsertNewAndUpdateOldSql sql语句 添加CDate 添加默认值

--- src/DotnetSpider.Extension/Pipeline/MySqlEntityPipeline.cs
+++ src/DotnetSpider.Extension/Pipeline/MySqlEntityPipeline.cs
@@ -210,7 +210,7 @@ namespace DotnetSpider.Extension.Pipeline
 			var tableName = adapter.Table.CalculateTableName();
 
 			var sql =
-				$"INSERT INTO `{adapter.Table.Database}`.`{tableName}` ({cols}) VALUES {colsParams} ON DUPLICATE KEY UPDATE {setParams};";
+				$"INSERT INTO `{adapter.Table.Database}`.`{tableName}` ({cols}) VALUES ({colsParams}) ON DUPLICATE KEY UPDATE {setParams};";
 
 			return sql;
 		}
