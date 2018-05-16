d6422806d372fe62ce782880bb9d4c3b54426689
邹嵩 <zousong@research.xbmail.com.cn>
2018-05-16 18:04:25 +0800

    test travis

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 dotnet --info
-dotnet build DotnetSpider.sln
+echo 'Run core tests'
 dotnet test src/DotnetSpider.Core.Test/DotnetSpider.Core.Test.csproj -f netcoreapp2.0
+echo 'Run extension tests'
 dotnet test src/DotnetSpider.Extension.Test/DotnetSpider.Extension.Test.csproj -f netcoreapp2.0
