efca19fba7f8d3b78270f3ef8ee1f36e8700a89b
邹嵩 <zousong@research.xbmail.com.cn>
2018-05-16 17:58:11 +0800

    update for travis

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 dotnet --info
 dotnet build DotnetSpider.sln
-dotnet test src/DotnetSpider.Core.Test/DotnetSpider.Core.Test.csproj -f netcoreapp2.0 -c Release
-dotnet test src/DotnetSpider.Extension.Test/DotnetSpider.Extension.Test.csproj -f netcoreapp2.0 -c Release
+dotnet test src/DotnetSpider.Core.Test/DotnetSpider.Core.Test.csproj -f netcoreapp2.0
+dotnet test src/DotnetSpider.Extension.Test/DotnetSpider.Extension.Test.csproj -f netcoreapp2.0
