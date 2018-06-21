7b3010c6d9e16bd6e396de0273341eb5a98d6533
邹嵩 <zousong@research.xbmail.com.cn>
2018-06-21 15:18:26 +0800

    upgrade runtest shell

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,5 @@
 dotnet --info
 echo 'Run core tests'
-dotnet test src/netstandard2.0/DotnetSpider.Core.Test/DotnetSpider.Core.Test.csproj -f netcoreapp2.0 -c release
+dotnet test src/netstandard2.0/DotnetSpider.Core.Test/DotnetSpider.Core.Test.csproj -f netcoreapp2.1 -c release
 echo 'Run extension tests'
-dotnet test src/netstandard2.0/DotnetSpider.Extension.Test/DotnetSpider.Extension.Test.csproj -f netcoreapp2.0 -c release
+dotnet test src/netstandard2.0/DotnetSpider.Extension.Test/DotnetSpider.Extension.Test.csproj -f netcoreapp2.1 -c release
