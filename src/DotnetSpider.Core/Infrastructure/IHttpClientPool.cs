9315ed842904a850c9a4bf08df3dea9f03ac723a
邹嵩 <zousong@research.xbmail.com.cn>
2018-07-10 14:19:26 +0800

    修正一个自动重定向的问题

--- src/DotnetSpider.Core/Infrastructure/IHttpClientPool.cs
+++ src/DotnetSpider.Core/Infrastructure/IHttpClientPool.cs
@@ -2,8 +2,6 @@
 using System.Net;
 using System.Net.Http;
 using System.Runtime.CompilerServices;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace DotnetSpider.Core.Infrastructure
 {
@@ -29,7 +27,7 @@ namespace DotnetSpider.Core.Infrastructure
 				AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip,
 				UseProxy = true,
 				UseCookies = true,
-				AllowAutoRedirect = allowAutoRedirect,
+				AllowAutoRedirect = false,
 				MaxAutomaticRedirections = 10
 			};
 			Client = allowAutoRedirect ? new HttpClient(new GlobalRedirectHandler(Handler)) : new HttpClient(Handler);
@@ -40,60 +38,6 @@ namespace DotnetSpider.Core.Infrastructure
 			Handler.CookieContainer = cookieContainerFactory();
 
 			_inited = true;
-		}
-
-		public class GlobalRedirectHandler : DelegatingHandler
-		{
-			public GlobalRedirectHandler(HttpMessageHandler innerHandler)
-			{
-				InnerHandler = innerHandler;
-			}
-
-			protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-			{
-				var response = await base.SendAsync(request, cancellationToken);
-
-				if (response.StatusCode == HttpStatusCode.MovedPermanently
-					|| response.StatusCode == HttpStatusCode.Moved
-					|| response.StatusCode == HttpStatusCode.Redirect
-					|| response.StatusCode == HttpStatusCode.Found
-					|| response.StatusCode == HttpStatusCode.SeeOther
-					|| response.StatusCode == HttpStatusCode.RedirectKeepVerb
-					|| response.StatusCode == HttpStatusCode.TemporaryRedirect
-					|| (int)response.StatusCode == 308)
-				{
-
-					var newRequest = CopyRequest(response.RequestMessage);
-
-					if (response.StatusCode == HttpStatusCode.Redirect
-						|| response.StatusCode == HttpStatusCode.Found
-						|| response.StatusCode == HttpStatusCode.SeeOther)
-					{
-						newRequest.Content = null;
-						newRequest.Method = HttpMethod.Get;
-					}
-					newRequest.RequestUri = new Uri(response.RequestMessage.RequestUri, response.Headers.Location);
-
-					response = await SendAsync(newRequest, cancellationToken);
-				}
-				return response;
-			}
-
-			private static HttpRequestMessage CopyRequest(HttpRequestMessage oldRequest)
-			{
-				var newrequest = new HttpRequestMessage(oldRequest.Method, oldRequest.RequestUri);
-
-				foreach (var header in oldRequest.Headers)
-				{
-					newrequest.Headers.TryAddWithoutValidation(header.Key, header.Value);
-				}
-				foreach (var property in oldRequest.Properties)
-				{
-					newrequest.Properties.Add(property);
-				}
-				if (oldRequest.Content != null) newrequest.Content = new StreamContent(oldRequest.Content.ReadAsStreamAsync().Result);
-				return newrequest;
-			}
 		}
 	}
 	/// <summary>
