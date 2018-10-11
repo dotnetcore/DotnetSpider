131aadde42b553a333245575ee69d71377ed0184
Lewis Zou <zlzforever@163.com>
2018-10-11 09:42:42 +0800

    FIX: 放开 Sqls 里的属性, 让用户可以扩展其它数据库

--- src/DotnetSpider.Extension/Pipeline/Sqls.cs
+++ src/DotnetSpider.Extension/Pipeline/Sqls.cs
@@ -5,26 +5,26 @@
 		/// <summary>
 		/// 插入的SQL语句
 		/// </summary>
-		internal string InsertSql { get; set; }
+		public string InsertSql { get; set; }
 
 		/// <summary>
 		/// 插入并且忽略重复数据的SQL
 		/// </summary>
-		internal string InsertAndIgnoreDuplicateSql { get; set; }
+		public string InsertAndIgnoreDuplicateSql { get; set; }
 
 		/// <summary>
 		/// 更新的SQL
 		/// </summary>
-		internal string UpdateSql { get; set; }
+		public string UpdateSql { get; set; }
 
 		/// <summary>
 		/// 查询的SQL
 		/// </summary>
-		internal string SelectSql;
+		public string SelectSql;
 
 		/// <summary>
 		/// 插入新的或者更新旧的数据SQL
 		/// </summary>
-		internal string InsertNewAndUpdateOldSql { get; set; }
+		public string InsertNewAndUpdateOldSql { get; set; }
 	}
 }
