bbeaa9341cdf7de85fab540fa1ae50790fe6f39e
Lewis Zou <zlzforever@163.com>
2019-03-30 19:59:05 +0800

    update test cases

--- tests/DotnetSpider.Tests/MessageQueue/LocalMessageQueueTests.cs
+++ tests/DotnetSpider.Tests/MessageQueue/LocalMessageQueueTests.cs
@@ -5,47 +5,72 @@ using Xunit;
 
 namespace DotnetSpider.Tests.MessageQueue
 {
-    public class LocalMessageQueueTests : TestBase
-    {
-        [Fact(DisplayName = "PubAndSub")]
-        public async Task PubAndSub()
-        {
-            int count = 0;
-            var mq = new LocalMessageQueue(null);
-            mq.Subscribe("topic", msg => { Interlocked.Increment(ref count); });
-            for (int i = 0; i < 1000; ++i)
-            {
-                await mq.PublishAsync("topic", "a");
-            }
+	public class LocalMessageQueueTests : TestBase
+	{
+		[Fact(DisplayName = "PubAndSub")]
+		public async Task PubAndSub()
+		{
+			int count = 0;
+			var mq = new LocalMessageQueue(null);
+			mq.Subscribe("topic", msg => { Interlocked.Increment(ref count); });
+			for (int i = 0; i < 100; ++i)
+			{
+				await mq.PublishAsync("topic", "a");
+			}
 
-            Thread.Sleep(2000);
-            Assert.Equal(1000, count);
-        }
+			int j = 0;
+			while (count != 100 && j < 300)
+			{
+				Thread.Sleep(500);
+				++j;
+			}
 
-        [Fact(DisplayName = "PubAndUnSub")]
-        public async Task PubAndUnSub()
-        {
-            int count = 0;
-            var mq = new LocalMessageQueue(null);
-            mq.Subscribe("topic", msg => { Interlocked.Increment(ref count); });
+			Assert.Equal(100, count);
+		}
 
-            int i = 0;
-            Task.Factory.StartNew(async () =>
-            {
-                for (; i < 100; ++i)
-                {
-                    await mq.PublishAsync("topic", "a");
-                    await Task.Delay(100);
-                }
-            }).ConfigureAwait(false).GetAwaiter();
-            await Task.Delay(3000);
-            mq.Unsubscribe("topic");
-            while (i < 100)
-            {
-                await Task.Delay(1000);
-            }
+		[Fact(DisplayName = "ParallelPubAndSub")]
+		public void ParallelPubAndSub()
+		{
+			int count = 0;
+			var mq = new LocalMessageQueue(null);
+			mq.Subscribe("topic", msg => { Interlocked.Increment(ref count); });
 
-            Assert.True(count < 100);
-        }
-    }
+			Parallel.For(0, 100, async (i) => { await mq.PublishAsync("topic", "a"); });
+			int j = 0;
+			while (count != 100 && j < 300)
+			{
+				Thread.Sleep(500);
+				++j;
+			}
+
+			Assert.Equal(100, count);
+		}
+
+		[Fact(DisplayName = "PubAndUnSub")]
+		public async Task PubAndUnSub()
+		{
+			int count = 0;
+			var mq = new LocalMessageQueue(null);
+			mq.Subscribe("topic", msg => { Interlocked.Increment(ref count); });
+
+			int i = 0;
+			Task.Factory.StartNew(async () =>
+			{
+				for (; i < 100; ++i)
+				{
+					await mq.PublishAsync("topic", "a");
+					await Task.Delay(100);
+				}
+			}).ConfigureAwait(false).GetAwaiter();
+			await Task.Delay(3000);
+			mq.Unsubscribe("topic");
+
+			while (i < 100)
+			{
+				await Task.Delay(500);
+			}
+
+			Assert.True(count < 100);
+		}
+	}
 }
